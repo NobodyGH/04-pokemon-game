5552cfa52850309830a717945e18ec5aeada0fd3
Nobody <efra123d456@gmail.com>
2022-03-29 15:48:01 -0400

    poke

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <h1>Quien es este pokemon?</h1>
+    
+    <pokemon-picture :pokemonId="4" :showPokemon="true" />
+
+    <PokemonOptions />
+
+</template>
+
+<script>
+
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+export default {
+  components: { PokemonPicture, PokemonOptions }
+}
+</script>
